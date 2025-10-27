7ebea0eacf3b4dfb4f74d6c69c7d805cc44801f3
Isaac <iabella.4576@gmail.com>
2025-10-27 07:25:09 -0400

    Doing Dr. Plank's TopCoder Problems as well

---
+++ tc/SRM_480/main.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <list>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Cryptography {
+public:
+  long long encrypt(vector<int> numbers);
+};
+
+long long Cryptography::encrypt(vector<int> numbers) { return 0; }
